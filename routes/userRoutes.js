96e98da8ff78783fdd6ad62418d4f8eb5f10fca3
dreamboyguru <143526836+dreamboyguru@users.noreply.github.com>
2023-11-30 18:54:53 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,6 +10,6 @@ router.post("/register", registerUser)
 
 router.post("/login",loginUser)
 
-router.get("/current", currentUser)
+router.get("/current", validateToken, currentUser)
  
 module.exports=router;
